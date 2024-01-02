2193e5873d90290c789f889db2923823fd59adf4
Clawiatura <maksimov19992805@icloud.com>
2024-01-02 12:38:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Что такое Комплиятор своими словами? ");
+Console.ReadLine();
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+//Console.WriteLine("1 13 49");
+
+
+//Console.WriteLine("13  44  22");
+
+
+Console.WriteLine("Two roads diverged in a yellow wood,\r\nAnd sorry I could not travel both\r\nAnd be one traveler, long I stood\r\nAnd looked down one as far as I could\r\nTo where it bent in the undergrowth.");
+
